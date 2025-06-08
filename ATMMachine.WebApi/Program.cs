992b94fe3f7dbd5c246e77b045b04a4758a6f637
naveen0125 <68naveendwivedi@gmail.com>
2025-06-09 00:26:52 +0530

    Updated CORS Policy

--- ATMMachine.WebApi/Program.cs
+++ ATMMachine.WebApi/Program.cs
@@ -7,12 +7,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IAccountService, AccountService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
